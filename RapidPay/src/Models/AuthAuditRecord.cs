a698e86fbbdd925e4ee9a0de63d48963e97ea4b5
similacra <sb.movieguy@gmail.com>
2025-02-16 09:42:26 -0600

    Got payment authorization added

---
+++ RapidPay/src/Models/AuthAuditRecord.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RapidPay.Models;
+
+public class AuthAuditRecord
+{
+    [Required]
+    public string CardNumber {get;set;}
+
+    public float Amount {get;set;}
+
+    public bool Approved {get;set;}
+
+    public string? DenialReason {get;set;}
+}
---
+++ RapidPay/src/Models/AuthorizationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace RapidPay.Models;
+
+public class AuthorizationResult
+{
+    public bool Authorized {get; set;}
+
+    public string? DenialReason {get;set;}
+}
