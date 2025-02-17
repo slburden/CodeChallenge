fc95058e7e28ec7014af9655c1a9fd175837fd2c
similacra <sb.movieguy@gmail.com>
2025-02-16 21:14:02 -0600

    Initial setup for JWT

--- RapidPay/src/Api/Controllers/CardController.cs
+++ RapidPay/src/Api/Controllers/CardController.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 using RapidPay.Business.Interfaces;
@@ -7,6 +6,7 @@ using RapidPay.Models;
 
 namespace RapidPay.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CardController : ControllerBase
--- RapidPay/src/Api/Program.cs
+++ RapidPay/src/Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 using RapidPay.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +12,51 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.RegisterApiServices();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RapidPay",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Bearer",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+   {
+     new OpenApiSecurityScheme
+     {
+       Reference = new OpenApiReference
+       {
+         Type = ReferenceType.SecurityScheme,
+         Id = "Bearer"
+       }
+      },
+      new string[] { }
+    }
+  });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(jwtOptions =>
+    {
+        jwtOptions.Events = new JwtBearerEvents() {
+
+        };
+        jwtOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            LogValidationExceptions = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidIssuer = "https://my-issuer", // your issuer,  
+            ValidAudience = "https://my-audience" // your audience
+        };
+    });
 
 var app = builder.Build();
 
